32b83c6e26d31457b0e9b53ff3912d0e4cfa6f71
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-28 09:28:47 -0300

    feat: implement runner at index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+const Runner = require('./runner')
+const runner = new Runner()
+
+const run = async () => {
+    const result = await runner.collectFiles();
+    console.log(result);
+}
+
+run()
+
 console.log('Running tests...');
